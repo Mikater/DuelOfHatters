87bf2881aa725bb7008de4bb24c0f5da23e134e4
Mikater <stepchik09012004@gmail.com>
2023-11-12 20:42:39 +0200

    Recoshet

--- DuelOfHetters_clone_0/Assets/Scripts/Bullet1.cs
+++ DuelOfHetters_clone_0/Assets/Scripts/Bullet1.cs
@@ -8,6 +8,8 @@ public class Bullet1 : MonoBehaviour
     public float timeToDestroy;
     private Rigidbody2D rb;
 
+    private Vector3 LastVelocity;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,11 +17,19 @@ public class Bullet1 : MonoBehaviour
         Invoke("TimeToDestroy", timeToDestroy);
     }
 
+    private void Update()
+    {
+        LastVelocity = rb.velocity;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Object"))
+        if (collision.gameObject.CompareTag("Reflector"))
         {
-            
+            var direction = Vector3.Reflect(LastVelocity.normalized, collision.contacts[0].normal);
+
+            rb.velocity = direction * speed;
+            return;
         }
         Invoke("TimeToDestroy", 0.01f);
     }
